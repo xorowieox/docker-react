36157bd04afb49c56d5cf3b6ecbbd1d691032a4e
Rowie Sou <rowie@Rowies-MacBook-Air.local>
2020-05-09 11:33:08 -0700

    Adding files to repo

---
+++ build/precache-manifest.56b73f2615478c31e5d99af74368e78a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be8db953ac30b77c93a0652e7d9dd04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d150f442643b9dcfa5ca",
+    "url": "/static/css/main.b02e1c38.chunk.css"
+  },
+  {
+    "revision": "b5b734056f93cf4f02d3",
+    "url": "/static/js/2.b1327c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1327c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d150f442643b9dcfa5ca",
+    "url": "/static/js/main.3e964674.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
